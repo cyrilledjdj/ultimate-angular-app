1d92ee5f1487e213b4a2cb6cdd0042a41032990b
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-12-15 22:54:03 -0500

    Initial configuration of Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AuthModule } from './auth/auth.module';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
 import { AppNavComponent } from './components/app-nav/app-nav.component';
 import { HealthModule } from './health/health.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
 	declarations: [ AppComponent, AppHeaderComponent, AppNavComponent ],
-	imports: [ BrowserModule, AppRoutingModule, AuthModule, HealthModule ],
+	imports: [ BrowserModule, AppRoutingModule, AuthModule, HealthModule, BrowserAnimationsModule ],
 	providers: [ Store ],
 	bootstrap: [ AppComponent ]
 })
