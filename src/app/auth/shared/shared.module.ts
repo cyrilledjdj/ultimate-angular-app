f2ff9866545f79e3cb7bcd3e96cd53334b9ae658
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-12-19 23:21:17 -0500

    Update login page to use angular material components

--- src/app/auth/shared/shared.module.ts
+++ src/app/auth/shared/shared.module.ts
@@ -1,6 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 import { AuthFormComponent } from './components/auth-form/auth-form.component';
 import { AuthGuard } from './guards/auth.guard';
@@ -8,8 +12,8 @@ import { AuthService } from './services/auth/auth.service';
 
 @NgModule({
 	declarations: [ AuthFormComponent ],
-	imports: [ CommonModule, ReactiveFormsModule ],
-	exports: [ AuthFormComponent ]
+	imports: [ CommonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatCardModule ],
+	exports: [ AuthFormComponent, MatButtonModule ]
 })
 export class SharedModule {
 	static forRoot(): ModuleWithProviders {
